174ccc39d34b9fb79a92c5ccc437f788b51a5e0d
Karim <karem109k@gmail.com>
2026-01-29 23:41:09 +0200

    Finish Category & Package Pages

--- src/pages/Manager/Rooms/Category.jsx
+++ src/pages/Manager/Rooms/Category.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Popup from "../../../components/Popup";
 import Input from "../../../components/Input";
 import Table from "../../../components/Table/Table";
@@ -6,20 +5,40 @@ import Section from "../../../components/Section";
 import TableRow from "../../../components/Table/TableRow";
 import TableData from "../../../components/Table/TableData";
 import TableEdit from "../../../components/Table/TableEdit";
+import { useState } from "react";
+import Button from "../../../components/Button";
+import useApi from "../../../../hooks/useApi";
+import api from "../../../../api/axios";
+import useTable from "../../../../hooks/useTable";
 
 const Category = () => {
-  const [data, setData] = useState([
-    { name: "Standard" },
-    { name: "Premium" },
-    { name: "Dulxe" },
-  ]);
-  const [editItem, setEditItem] = useState(null);
-  const [mode, setMode] = useState(null);
+  const {
+    data,
+    setData,
+    loading,
+    mode,
+    setMode,
+    setEditItem,
+    editItem,
+    paginationData,
+    next,
+    prev,
+    setFilters,
+  } = useTable("roomCategory");
 
   return (
     <>
       <Section extraPadding classname="p-5 w-full">
-        <Table head={["Name", "Edit"]} setMode={setMode} showFilters showAdd>
+        <Table
+          head={["Name", "Edit"]}
+          setMode={setMode}
+          loading={loading}
+          pagenationData={paginationData}
+          next={next}
+          prev={prev}
+          showFilters
+          showAdd
+        >
           {data.map((ele, idx) => (
             <TableRow key={idx} rowNum={idx}>
               <TableData>{ele.name}</TableData>
@@ -31,33 +50,142 @@ const Category = () => {
           ))}
         </Table>
       </Section>
-      <Filters mode={mode} setMode={setMode} />
-      <Add mode={mode} setMode={setMode} />
-      <Edit mode={mode} setMode={setMode} />
+      {mode === "Filters" && (
+        <Filters mode={mode} setMode={setMode} setFilters={setFilters} />
+      )}
+      {mode === "Add" && (
+        <Add
+          mode={mode}
+          setMode={setMode}
+          setData={setData}
+          dataLength={data.length}
+        />
+      )}
+      {mode === "Edit" && (
+        <Edit
+          mode={mode}
+          setMode={setMode}
+          editItem={editItem}
+          setEditItem={setEditItem}
+        />
+      )}
     </>
   );
 };
 
-const Filters = ({ mode, setMode }) => {
+const Filters = ({ mode, setMode, setFilters }) => {
+  const [name, setName] = useState("");
   return (
-    <Popup title={mode} open={mode === "Filters"} setMode={setMode}>
-      <Input title="Name" />
+    <Popup title={mode} setMode={setMode}>
+      <div className="flex">
+        <Input title="Name" value={name} setValue={setName} />
+      </div>
+      <Button
+        full
+        onClick={() => {
+          const obj = {};
+          if (name) obj.name = name;
+          setFilters(obj);
+          setMode(null);
+        }}
+      >
+        Search
+      </Button>
     </Popup>
   );
 };
 
-const Add = ({ mode, setMode }) => {
+const Add = ({ mode, setMode, setData, dataLength }) => {
+  const [name, setName] = useState();
+  const { globalErrors, loading, request } = useApi((payload) =>
+    api.post("roomCategory", payload),
+  );
+  console.log(globalErrors);
+  const handleCreate = async () => {
+    const { data, ok } = await request({ name });
+    if (ok) {
+      if (dataLength >= 10) {
+        setData((prev) => {
+          const copy = [...prev];
+          copy.pop();
+          copy.unshift(data.data);
+          return copy;
+        });
+      } else {
+        setData((prev) => [...prev, data.data]);
+      }
+      setName("");
+      setMode(null);
+    }
+  };
   return (
-    <Popup title={mode} open={mode === "Add"} setMode={setMode}>
-      <Input title="Name" />
+    <Popup title={mode} setMode={setMode} globalErrors={globalErrors}>
+      <div className="flex">
+        <Input title="Name" value={name} setValue={setName} />
+      </div>
+      <Button disabled={loading || !name} onClick={handleCreate} full>
+        Create
+      </Button>
     </Popup>
   );
 };
 
-const Edit = ({ mode, setMode, editItem }) => {
+const Edit = ({ mode, setMode, editItem, setEditItem }) => {
+  const [name, setName] = useState(editItem.name);
+  const [isSure, setIsSure] = useState(false);
+
+  const { globalErrors, request, loading } = useApi((payload) =>
+    api.put(`roomCategory/${editItem._id}`, payload),
+  );
+
+  const HandleDelete = async () => {
+    if (!isSure) {
+      setIsSure(true);
+      return;
+    }
+    const res = await api.delete(`roomCategory/${editItem._id}`);
+    setMode(null);
+    setEditItem(null);
+  };
+
+  const handleUpdate = async () => {
+    if (editItem.name === name) return;
+    const { ok } = await request({ name });
+    if (ok) {
+      setMode(null);
+      setEditItem(null);
+    }
+  };
   return (
-    <Popup title={mode} open={mode === "Edit"} setMode={setMode}>
-      <Input title="Name" />
+    <Popup title={mode} setMode={setMode} globalErrors={globalErrors}>
+      <div className="flex">
+        <Input title="Name" value={name} setValue={setName} />
+      </div>
+      <Button
+        full
+        onClick={handleUpdate}
+        disabled={loading || !name || editItem.name === name}
+      >
+        Update
+      </Button>
+      {!isSure && (
+        <Button
+          full={true}
+          onClick={HandleDelete}
+          className="bg-red-600 hover:bg-red-700"
+        >
+          Delete
+        </Button>
+      )}
+      {isSure && (
+        <Button
+          full={true}
+          onClick={HandleDelete}
+          className="bg-red-600 hover:bg-red-700"
+        >
+          [DELETE] ARE YOU SURE!!!!! [DELETE]
+        </Button>
+      )}
     </Popup>
   );
 };
--- src/pages/Manager/Rooms/Package.jsx
+++ src/pages/Manager/Rooms/Package.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Section from "../../../components/Section";
 import Table from "../../../components/Table/Table";
 import Popup from "../../../components/Popup";
@@ -6,15 +5,26 @@ import Input from "../../../components/Input";
 import TableRow from "../../../components/Table/TableRow";
 import TableData from "../../../components/Table/TableData";
 import TableEdit from "../../../components/Table/TableEdit";
+import { useState } from "react";
+import Button from "../../../components/Button";
+import useApi from "../../../../hooks/useApi";
+import api from "../../../../api/axios";
+import useTable from "../../../../hooks/useTable";
 
 const Package = () => {
-  const [data, setData] = useState([
-    { name: "Breakfast", price: 10 },
-    { name: "Dinner", price: 10 },
-    { name: "Full", price: 10 },
-  ]);
-  const [editItem, setEditItem] = useState(null);
-  const [mode, setMode] = useState(null);
+  const {
+    data,
+    setData,
+    loading,
+    mode,
+    setMode,
+    setEditItem,
+    editItem,
+    paginationData,
+    next,
+    prev,
+    setFilters,
+  } = useTable("packages");
 
   return (
     <>
@@ -22,6 +32,10 @@ const Package = () => {
         <Table
           head={["Name", "Price", "Edit"]}
           setMode={setMode}
+          loading={loading}
+          pagenationData={paginationData}
+          next={next}
+          prev={prev}
           showFilters
           showAdd
         >
@@ -37,36 +51,149 @@ const Package = () => {
           ))}
         </Table>
       </Section>
-      <Filters mode={mode} setMode={setMode} />
-      <Add mode={mode} setMode={setMode} />
-      <Edit mode={mode} setMode={setMode} />
+      {mode === "Filters" && (
+        <Filters mode={mode} setMode={setMode} setFilters={setFilters} />
+      )}
+      {mode === "Add" && (
+        <Add
+          mode={mode}
+          setMode={setMode}
+          setData={setData}
+          dataLength={data.length}
+        />
+      )}
+      {mode === "Edit" && (
+        <Edit
+          mode={mode}
+          setMode={setMode}
+          editItem={editItem}
+          setEditItem={setEditItem}
+        />
+      )}
     </>
   );
 };
 
-const Filters = ({ mode, setMode }) => {
+const Filters = ({ mode, setMode, setFilters }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState();
   return (
-    <Popup title={mode} open={mode === "Filters"} setMode={setMode}>
-      <Input title="Name" />
-      <Input title="Price" />
+    <Popup title={mode} setMode={setMode}>
+      <div className="flex gap-5">
+        <Input title="Name" value={name} setValue={setName} />
+        <Input title="Price" value={price} setValue={setPrice} />
+      </div>
+      <Button
+        full
+        onClick={() => {
+          const obj = {};
+          if (name) obj.name = name;
+          if(price >= 0) obj.price = price
+          setFilters(obj);
+          setMode(null);
+        }}
+      >
+        Search
+      </Button>
     </Popup>
   );
 };
 
-const Add = ({ mode, setMode }) => {
+const Add = ({ mode, setMode, setData, dataLength }) => {
+  const [name, setName] = useState();
+  const [price, setPrice] = useState(0);
+  const { globalErrors, loading, request } = useApi((payload) =>
+    api.post("packages", payload),
+  );
+  console.log(globalErrors);
+  const handleCreate = async () => {
+    const { data, ok } = await request({ name, price });
+    if (ok) {
+      if (dataLength >= 10) {
+        setData((prev) => {
+          const copy = [...prev];
+          copy.pop();
+          copy.unshift(data.data);
+          return copy;
+        });
+      } else {
+        setData((prev) => [...prev, data.data]);
+      }
+      setName("");
+      setMode(null);
+    }
+  };
   return (
-    <Popup title={mode} open={mode === "Add"} setMode={setMode}>
-      <Input title="Name" />
-      <Input title="Price" />
+    <Popup title={mode} setMode={setMode} globalErrors={globalErrors}>
+      <div className="flex gap-5">
+        <Input title="Name" value={name} setValue={setName} />
+        <Input title="Price" value={price} setValue={setPrice} />
+      </div>
+      <Button disabled={loading || !name || price < 0} onClick={handleCreate} full>
+        Create
+      </Button>
     </Popup>
   );
 };
 
-const Edit = ({ mode, setMode, editItem }) => {
+const Edit = ({ mode, setMode, editItem, setEditItem }) => {
+  const [name, setName] = useState(editItem.name);
+  const [price, setPrice] = useState(editItem.price);
+  const [isSure, setIsSure] = useState(false);
+
+  const { globalErrors, request, loading } = useApi((payload) =>
+    api.put(`packages/${editItem._id}`, payload),
+  );
+
+  const HandleDelete = async () => {
+    if (!isSure) {
+      setIsSure(true);
+      return;
+    }
+    const res = await api.delete(`packages/${editItem._id}`);
+    setMode(null);
+    setEditItem(null);
+  };
+
+  const handleUpdate = async () => {
+    if (editItem.name === name && editItem.price === price) return;
+    const { ok } = await request({ name, price });
+    if (ok) {
+      setMode(null);
+      setEditItem(null);
+    }
+  };
   return (
-    <Popup title={mode} open={mode === "Edit"} setMode={setMode}>
-      <Input title="Name" />
-      <Input title="Price" />
+    <Popup title={mode} setMode={setMode} globalErrors={globalErrors}>
+      <div className="flex gap-5">
+        <Input title="Name" value={name} setValue={setName} />
+        <Input title="Price" value={price} setValue={setPrice} />
+      </div>
+      <Button
+        full
+        onClick={handleUpdate}
+        disabled={loading || !name || (editItem.name === name && editItem.price === price)}
+      >
+        Update
+      </Button>
+      {!isSure && (
+        <Button
+          full={true}
+          onClick={HandleDelete}
+          className="bg-red-600 hover:bg-red-700"
+        >
+          Delete
+        </Button>
+      )}
+      {isSure && (
+        <Button
+          full={true}
+          onClick={HandleDelete}
+          className="bg-red-600 hover:bg-red-700"
+        >
+          [DELETE] ARE YOU SURE!!!!! [DELETE]
+        </Button>
+      )}
     </Popup>
   );
 };
